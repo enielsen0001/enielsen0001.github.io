73c5d1100efa8bc563d3f0148fb4c340d80dfe1f
Erika Nielsen <enielsen0001@gmail.com>
2024-11-19 13:27:04 -0500

    update resume and add link to repo

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -16,15 +16,18 @@ const ProjectsPage = () => {
             <div className={styles.proj}>
                 <h2>Portfolio website (this site)</h2>
                 <p>Featured skills: HTML, SCSS, JavsScript, React, Typescript, Bootstrap</p>
-                <p>This is a simple, clean and functional personal portfolio website built with React and hosted on GitHub Pages. It showcases my skills, experience, and projects in a user-friendly and visually appealing manner.</p>
+                <p>This is a simple, clean and functional personal portfolio website built with React and hosted on GitHub Pages.</p>
 
                 <ArrowLink href="https://github.com/enielsen0001/enielsen0001.github.io">See Portfolio site <span className="sr-only"> on GitHub</span> <FontAwesomeIcon icon={faGithub} /></ArrowLink>
             </div>
             <div className={styles.proj}>
                 <h2>MERN Stack Notes App - <FontAwesomeIcon icon={faScrewdriverWrench} /> WIP</h2>
-                <p>Featured skills: MongoDb, Mongoose, Express, Node, JSON Web Tokens (JWT), React</p>
+                <p>Featured skills: MongoDb, Mongoose, Express, Node, JSON Web Tokens (JWT), React, Redux, Typescript</p>
                 <p>The 'Hello world' of full stack JS development.</p>
-                <p>This is a full-stack web application built using the MERN stack (MongoDB, Express.js, React, and Node.js). It allows users to create, edit, and delete notes, providing a simple and efficient way to organize thoughts and ideas.</p></div>
+                <p>This is a full-stack web application built using the MERN stack (MongoDB, Express.js, React, and Node.js). It allows users to create, edit, and delete notes, providing a simple and efficient way to organize thoughts and ideas.</p>
+
+                <ArrowLink href="https://github.com/enielsen0001/enielsen0001.github.io">See notes app <span className="sr-only"> on GitHub</span> <FontAwesomeIcon icon={faGithub} /></ArrowLink>
+            </div>
 
             <div className={styles.proj}>
                 <h2>D3js Examples - <FontAwesomeIcon icon={faScrewdriverWrench} /> WIP</h2>
