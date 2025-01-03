8cd38023911ff446028a5bfc5a55de42a6705efe
Erika Nielsen <enielsen0001@gmail.com>
2025-01-03 13:27:30 -0500

    update resume

--- src/components/global/PageIntro.tsx
+++ src/components/global/PageIntro.tsx
@@ -10,7 +10,7 @@ const PageIntro = ({ className, children }: TPageIntroProps) => {
         <div className={`page-intro ${className}`}>
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-10">
+                    <div className="col-lg-10">
                         { children}
                     </div>
                 </div>
