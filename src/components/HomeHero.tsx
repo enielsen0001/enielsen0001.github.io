3bf9d10f0df1ee04d7fd49b1cde8df30ddb94e2d
Erika Nielsen <enielsen0001@gmail.com>
2025-01-23 16:56:28 -0500

    update home hero styles

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -6,7 +6,7 @@ const HomeHero = () => {
             <HeroBanner fileTitle="tunnel-hero" />
             <div className="home-hero__content container">
                 <h1 className="home-hero__hl"><span>Erika Nielsen</span></h1>
-                <p className="home-hero__sub-hl">Helping businesses and brands shine online.</p>
+                <p className="home-hero__sub-hl">Helping businesses and brands shine online</p>
             </div>
         </div>
     );
